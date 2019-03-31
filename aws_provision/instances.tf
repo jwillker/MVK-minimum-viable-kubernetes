da7d5a55f9859ef79729ba60d897c80dac074c01
Jhonn Willker <frazao.jhonn@gmail.com>
2019-03-30 23:24:25 -0300

    Better organization files add Packer and ansible provision

--- aws_provision/instances.tf
+++ aws_provision/instances.tf
@@ -1,23 +1,54 @@
+data "aws_ami" "k8s-base" {
+  most_recent = true
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["K8s-base-Packer-Ansible"]
+  }
+  owners    = ["967724168372"]
+}
+
 resource "aws_instance" "k8s_masters" {
-  ami           = "ami-0565af6e282977273"
-  instance_type = "t2.medium"
+  ami           = "${data.aws_ami.k8s-base.id}"
+  instance_type = "t2.micro"
   count         = 1
-  vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+  vpc_security_group_ids = [
+    "${aws_security_group.k8s-ssh.id}",
+    "${aws_security_group.k8s-master-sg.id}"
+  ]
+
   key_name      = "${var.AWS_SSH_KEY_NAME}"
-  
+
   tags {
     Name = "k8s-master-${count.index}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
 resource "aws_instance" "k8s_nodes" {
-  ami           = "ami-0565af6e282977273"
-  instance_type = "t2.medium"
+  ami           = "${data.aws_ami.k8s-base.id}"
+  instance_type = "t2.micro"
   count         = 2
-  vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+  vpc_security_group_ids = [
+    "${aws_security_group.k8s-ssh.id}",
+    "${aws_security_group.k8s-node-sg.id}"
+  ]
+
   key_name      = "${var.AWS_SSH_KEY_NAME}"
-  
+
   tags {
     Name = "k8s-node-${count.index}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
--- aws_provision/security_groups.tf
+++ aws_provision/security_groups.tf
@@ -1,10 +1,74 @@
-resource "aws_security_group" "kubernetes" {
-  name = "kubernetes-sg"
+resource "aws_security_group" "k8s-ssh" {
+  name = "k8s-ssh"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "k8s-master-sg" {
+  name = "master-k8s-api-server"
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Kubernetes API server"
+  }
+
+  ingress {
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "etcd server client API"
+  }
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Kubelet API"
+  }
+
+  ingress {
+    from_port   = 10251
+    to_port     = 10251
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "kube-scheduler"
+  }
+
+  ingress {
+    from_port   = 10252
+    to_port     = 10252
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "kube-controller-manager"
+  }
+}
+
+resource "aws_security_group" "k8s-node-sg" {
+  name = "k8s-node-sg"
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Kubelet API"
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NodePorts"
   }
 }
