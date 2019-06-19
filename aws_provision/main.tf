ae5ec61ed6af57de3482674779c1b366ca8d59a4
Jhonn Willker <frazao.jhonn@gmail.com>
2019-06-18 21:05:11 -0300

    Remove terraform aws provider variables and add packer variables

--- aws_provision/main.tf
+++ aws_provision/main.tf
@@ -1,18 +1,17 @@
 
 # Configure the AWS Provider
 provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-  region     = "${var.AWS_DEFAULT_REGION}"
+  region = "us-west-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "devopxlabs-terraform-state"
-    key    = "kubernetes/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# Need provision a s3 bucket before to use:
+#terraform {
+#  backend "s3" {
+#    bucket = "devopxlabs-terraform-state"
+#    key    = "kubernetes/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
 
 
 
